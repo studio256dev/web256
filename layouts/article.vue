845f74d1963e85bd663fdf93079f5d4966145426
DmitryGolubev <dmitrii.golubev@s256.ru>
2022-09-28 14:36:19 +0300

    добавлен лейаут для кейсов

---
+++ layouts/article.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="base-layout">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import '../assets/css/article-editor.min.css';
+@import '../assets/scss/custom-article-editor';
+
+.base-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+  background: $color-main-graphen;
+}
+
+</style>
