2f2e2591fddb7fe4902767613cca1fd639d7be44
DmitryGolubev <dmitrii.golubev@s256.ru>
2022-09-29 12:04:55 +0300

    Добавлены стили для article-editor

--- layouts/article.vue
+++ layouts/article.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="base-layout">
-    <Nuxt />
+  <div class="article-layout">
+    <div class="article-layout__wrapper">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
@@ -12,12 +14,16 @@ export default {}
 @import '../assets/css/article-editor.min.css';
 @import '../assets/scss/custom-article-editor';
 
-.base-layout {
+.article-layout {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
   overflow: hidden;
   background: $color-main-graphen;
-}
 
+  &__wrapper {
+    flex: 1 1 auto;
+    max-width: 800px;
+  }
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -83,7 +83,7 @@ export default {
   },
 
   generate: {
-    fallback: true
+    fallback: true,
   },
 
   yandexMetrika: {
